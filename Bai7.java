86e6a8752779bbee9968a31f0df3ae50b5ea570b
vannguyen <hoangvan060898@gmail.com>
2022-01-18 10:48:00 +0700

    f

--- Bai7.java
+++ Bai7.java
@@ -10,11 +10,9 @@ public class Bai7 {
         String str1 = scanner.nextLine();
         System.out.print("Nhập vào xâu s2: ");
         String str2 = scanner.nextLine();
-//        if (str1.contains(str2))
-//        {
-//            str1 =str1.replace(str2,"");
-//        }
-
+        if (str1.contains(str2)) {
+            str1 = str1.replace(str2, "");
+        }
         System.out.println( str1);
 
     }
